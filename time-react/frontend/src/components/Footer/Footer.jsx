be60fe7224d054f55dc8e894fd191c4a8903f1a2
Hossain Belaeat <s2400787@BC-Q4CJGD4M23-Kannettava-tietokone.local>
2024-11-29 12:50:32 +0200

    fix: footer.jsx

--- time-react/frontend/src/components/Footer/Footer.jsx
+++ time-react/frontend/src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
-import './Footer.css'
-import currentYear from '../../../src/utilits/currentYear'
+import "./Footer.css";
+import currentYear from "../../../src/utilits/currentYear";
 
-
-function Footer(){
-    return(
-        <p>
-            &copy; <span id="currentYear">{currentYear()}</span> timeShifters team (aka Belaeat Hossain, Saara Rikama, Stefanie Jana, Vladimir Beliakov)
-        </p>
-    );
+function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; <span id="currentYear">{currentYear()}</span> timeShifters team
+        (aka Belaeat Hossain, Saara Rikama, Stefanie Jana, Vladimir Beliakov)
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
