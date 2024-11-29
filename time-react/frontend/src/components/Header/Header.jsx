5c81cccd8dc1d4db93044f4d1f5c83f54cfd5a71
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-11-29 12:40:33 +0200

    refactorgameplay.jsx and gameplay.css

--- time-react/frontend/src/components/Header/Header.jsx
+++ time-react/frontend/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import './Header.css'
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
 
 function Header() {
     return (
         <header>
             <div id="logo">
-                <img src={logo} alt="TimeLink-game logo"/>
+                <img src={logo} alt="TimeLink-game logo" />
             </div>
             <h1>TimeLink</h1>
         </header>
